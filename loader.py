a8a9238c696201d73026cda7b1ecec92d73db8a9
Stanislav <71723405+HunterOnline@users.noreply.github.com>
2024-02-09 20:18:51 +0200

    Update loader.py

--- loader.py
+++ loader.py
@@ -5,6 +5,13 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from data import config
 from data.config import WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV
+from utils.db_api.db_gino import db
+
+bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
@@ -13,3 +20,6 @@ dp = Dispatcher(bot, storage=storage)
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 SSL_CERTIFICATE = open(WEBHOOK_SSL_CERT, "rb").read()
 ssl_context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
+
+
+__all__ = ["bot", "storage", "dp", "db"]
